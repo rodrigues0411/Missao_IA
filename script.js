7ffc8a796090d0df9155765c221a1a53fd9a74fe
Aline J Rodrigues <jesus.rodrigues.aline@escola.pr.gov.br>
2025-08-04 11:53:38 -0300

    ycvy

---
+++ script.js
@@ -0,0 +1,23 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+
+const caixaAlternativas = document.querySelector('.caixa-alternativas');
+
+const caixaResultado = document.querySelector('.caixa-resultado');
+
+const textoResultado = document.querySelector('.texto-Resultado');
+
+const lista = [];
+
+const perguntas [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [ "Alternativa 1","Alternativas 2" ],
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [ "Alternativa 1", "Alternativas 2" ],
+},
+ 
+]; // fecha lista
